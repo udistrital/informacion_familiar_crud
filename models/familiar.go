26f88da8b6675e1ed9b8128999f864d2ff413cba
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-08-07 12:37:38 -0500

    Especificar la relacion Fk en Servicios

--- models/familiar.go
+++ models/familiar.go
@@ -54,7 +54,7 @@ func GetFamiliarById(id int) (v *Familiar, err error) {
 func GetAllFamiliar(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(Familiar))
+	qs := o.QueryTable(new(Familiar)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
