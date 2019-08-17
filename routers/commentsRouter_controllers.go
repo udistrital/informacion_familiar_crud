b215da2afa834cec423360546f26323b918121c9
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-08-16 19:48:01 -0500

    Se agregra las rutas del nuevo modelo y controlado acudinete

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,6 +7,51 @@ import (
 
 func init() {
 
+    beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"] = append(beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"] = append(beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"] = append(beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"] = append(beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"] = append(beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:AcudienteController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
     beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:FamiliarController"] = append(beego.GlobalControllerRouter["github.com/udistrital/informacion_familiar_crud/controllers:FamiliarController"],
         beego.ControllerComments{
             Method: "Post",
